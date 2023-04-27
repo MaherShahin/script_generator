e5769f78c9ba859d0da8b82e5f21d6336de37577
MaherShahin <abdomaher2010@hotmail.com>
2023-04-27 02:26:50 +0200

    fixed validator and script class + updated reqs

--- script_validator.py
+++ script_validator.py
@@ -4,10 +4,22 @@ class ScriptValidator:
     def __init__(self):
         pass
 
-    def validate(self, shell_script):
-        try:
-            # Run the shell script with the 'bash -n' command, which checks the syntax without executing the script
-            result = subprocess.run(["bash", "-n"], input=shell_script, text=True, capture_output=True)
-            return result.returncode == 0
-        except subprocess.CalledProcessError:
+    def validate(self, code):
+        # Check for the presence of the shebang line
+        shebang_line = "#!/bin/bash"
+        if shebang_line not in code:
             return False
+
+        # Check for syntax errors using the 'bash -n' command
+        try:
+            with subprocess.Popen(["bash", "-n"], stdin=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
+                _, stderr = proc.communicate(input=code.encode())
+
+            if stderr:
+                print(f"Syntax errors found:\n{stderr.decode()}")
+                return False
+        except Exception as e:
+            print(f"Error occurred while checking syntax: {e}")
+            return False
+
+        return True
--- shell_script.py
+++ shell_script.py
@@ -2,15 +2,40 @@ import os
 import re
 
 class ShellScript:
+    
     def __init__(self, code):
         self.code = code
 
     def sanitize(self):
-        self.code = re.sub(r'[^#\w\s\.\-/]+', '', self.code)
+        # Remove text before and including the start delimiter (```bash)
+        start_delimiter = "```bash"
+        start_index = self.code.find(start_delimiter)
+        if start_index == -1:
+            raise ValueError("Start delimiter (```bash) not found in the code.")
+        self.code = self.code[start_index + len(start_delimiter):]
 
-    def save(self, directory="generated_scripts", filename="script.sh"):
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+        # Remove text after and including the end delimiter (```)
+        end_delimiter = "```"
+        end_index = self.code.rfind(end_delimiter)
+        if end_index == -1:
+            raise ValueError("End delimiter (```) not found in the code.")
+        self.code = self.code[:end_index]
 
-        with open(os.path.join(directory, filename), "w") as script_file:
-            script_file.write(self.code)
+        # Remove any leading and trailing whitespace
+        self.code = self.code.strip()
+
+        # Check for any potentially harmful commands
+        harmful_commands = ["rm ", "mv ", "dd ", "shutdown", "reboot", "init ", "kill"]
+        for cmd in harmful_commands:
+            if cmd in self.code:
+                raise ValueError(f"Harmful command '{cmd}' detected in the code. Please review the script and remove or replace it before running.")
+
+
+        
+    def save(self, path="generated_scripts/script.sh"):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, "w") as f:
+            f.write(self.code)
+
+    def __str__(self):
+        return f"\nHere is the shell script with comments as requested:\n\n{self.code}\n"
